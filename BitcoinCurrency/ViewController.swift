cc344f947a77c54d653ce0b02283d72895b86bf9
NijatNaghiyev <nicat614@icloud.com>
2025-01-20 14:41:31 +0400

    Initial Commit

---
+++ BitcoinCurrency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitcoinCurrency
+//
+//  Created by Nijat Naghiyev on 20.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
